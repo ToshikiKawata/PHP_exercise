6a9cebdf5aa5f5e1948c09255def2cf1887add23
ToshikiKawata <jubei.animal@gmail.com>
2021-09-15 09:25:19 +0900

    配列と連想配列の課題完了

---
+++ exercise/02/if_5.php
@@ -0,0 +1,16 @@
+<?php
+    $num = 10;
+
+    $bool = true;
+    for($i=2;$i<$num;$i++) {
+          if($num%$i == 0) {
+                $bool = false;
+                break;
+          }
+    }
+
+    if ($bool) {
+          echo $num."は素数です。";
+    } else {
+          echo $num."は素数ではありません。";
+    }
